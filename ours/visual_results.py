bff70213e5efaccb486cd089cac9f31af3b2d15c
NoFish-528 <nzk020109@163.com>
2022-09-11 19:28:55 +0800

    add visual results

--- ours/visual_results.py
+++ ours/visual_results.py
@@ -38,6 +38,13 @@ def main(file_path,output_dir):
 
 
 if __name__ == '__main__':
-    file_path = "/home/public/datasets/InternationalRaceTrackDataset/chusai_release/train/labels"
-    output_dir = "./dye_train_img_results/"
-    main(file_path,output_dir)
+    # file_path = "/home/public/datasets/InternationalRaceTrackDataset/chusai_release/train/labels"
+    # output_dir = "./dye_train_img_results/"
+    # main(file_path,output_dir)
+    res = cv2.imread("res.png",0)
+    print(res)
+    res_canny = cv2.imread("/home/public/datasets/InternationalRaceTrackDataset/chusai_release/train/labels/2.png",0)
+    print(res_canny)
+    d = np.argwhere(res != res_canny)
+    print(len(d))
+    print(512*512)
